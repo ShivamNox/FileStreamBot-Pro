d475cafcf9b16d2170cb1efd458cad2f903ebe9e
Shivam Kumar <shivamnox0@gmail.com>
2025-12-28 16:42:48 +0530

    Update __init__.py

--- ShivamNox/__init__.py
+++ ShivamNox/__init__.py
@@ -3,12 +3,8 @@
 import time
 import logging
 
-# Suppress noisy loggers at module level
+# Suppress warnings
 logging.getLogger("asyncio").setLevel(logging.WARNING)
-logging.getLogger("aiohttp").setLevel(logging.WARNING)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.session").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.connection").setLevel(logging.WARNING)
 
 StartTime = time.time()
 __version__ = "2.0.0"
